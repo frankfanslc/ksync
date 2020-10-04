48a530e23d0c2f41dad6c43ec393abaa76e7cd5c
donorp <donorp@arhat.solutions>
2020-10-05 00:17:38 +0200

    Upgrade kind to v0.9.0

--- scripts/test/e2e.mk
+++ scripts/test/e2e.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+e2e.v1-19:
+	sh scripts/test/e2e.sh v1-19
+
 e2e.v1-18:
 	sh scripts/test/e2e.sh v1-18
 
